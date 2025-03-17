0b385a83f48ac6a7d7ebba44f64b6dc19262b51e
AngelaYu-3 <yuangela12@gmail.com>
2025-03-16 18:30:59 -0700

    html_css_js

---
+++ html_css_js/script.js
@@ -0,0 +1,13 @@
+console.log('javscript is cool')
+
+var number = 10;
+var string = 'hello there'
+var is_cool = true;
+
+if (number == 10) {
+    console.log('is 10')
+} else {
+    console.log('not 10')
+}
+
+document.getElementById('box').innerHTML = string;
